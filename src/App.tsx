b949f87865a73f6fd144ddb965a7dcd1cd4a2513
Yena-Yun <cheryleduart@gmail.com>
2023-04-03 17:22:35 +0900

    redux 설치 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <div>
+    <div className='content'>
       <RouterProvider router={router} />
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from 'store';
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const store = createStore(rootReducer);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
