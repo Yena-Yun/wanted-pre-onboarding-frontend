93c9b330a85077ba4f8b761301c36e5f9117cd3b
Yena-Yun <cheryleduart@gmail.com>
2023-04-03 16:28:07 +0900

    CRA 내용을 client 폴더 밖으로 꺼냄

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Home } from 'pages/Home';
+import { Login } from 'pages/Login';
+import { Register } from 'pages/Register';
+import { Todo } from 'pages/Todo';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/signin',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Register />,
+  },
+  {
+    path: '/todo',
+    element: <Todo />,
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
