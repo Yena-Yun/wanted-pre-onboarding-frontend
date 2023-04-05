6e9de5330dbbf233388baa9c36c34862f31af25a
Yena-Yun <cheryleduart@gmail.com>
2023-04-05 16:39:13 +0900

    로컬스토리지 훅 생성

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+// export const getFromLocalStorage = (key: string) => {
+//   return localStorage.getItem(key);
+// };
+
+// export const saveToLocalStorage = (key: string, value: string) => {
+//   return localStorage.setItem(key, value);
+// };
+
+export const useLocalStorage = (keyName: string, defaultValue: any) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const value = window.localStorage.getItem(keyName) || null;
+
+      if (value) {
+        return JSON.parse(value);
+      } else {
+        window.localStorage.setItem(keyName, JSON.stringify(defaultValue));
+
+        return defaultValue;
+      }
+    } catch (err) {
+      console.log('로컬스토리지를 사용하는 도중 에러가 났습니다.');
+      return defaultValue;
+    }
+  });
+
+  const setValue = (newValue: any) => {
+    try {
+      window.localStorage.setItem(keyName, JSON.stringify(newValue));
+    } catch (err) {
+      console.log('로컬스토리지 저장에 실패했습니다.');
+    }
+
+    setStoredValue(newValue);
+  };
+
+  return [storedValue, setValue] as [string, (prev: string) => void];
+};
