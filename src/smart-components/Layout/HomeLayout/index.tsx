56a27c6a1a283b050e778001f2da2777df7c7d2c
Yena-Yun <cheryleduart@gmail.com>
2023-04-05 17:24:05 +0900

    Layout 컴포넌트 모아서 export

---
+++ src/smart-components/Layout/HomeLayout/index.tsx
@@ -0,0 +1,14 @@
+import { Navigate, useOutlet } from 'react-router-dom';
+import { useAuth } from 'hooks/useAuth';
+
+export const HomeLayout = () => {
+  const outlet = useOutlet();
+  const { token } = useAuth();
+
+  if (token) {
+    alert('자동 로그인이 되었습니다! 투두 페이지로 이동합니다.');
+    return <Navigate to='/todo' />;
+  }
+
+  return <div>{outlet}</div>;
+};
---
+++ src/smart-components/Layout/ProtectedLayout/index.tsx
@@ -0,0 +1,14 @@
+import { Navigate, useOutlet } from 'react-router-dom';
+import { useAuth } from 'hooks/useAuth';
+
+export const ProtectedLayout = () => {
+  const outlet = useOutlet();
+  const { token } = useAuth();
+
+  if (!token) {
+    alert('로그인이 되지 않았습니다. 로그인 페이지로 이동합니다.');
+    return <Navigate to='/signin' />;
+  }
+
+  return <div>{outlet}</div>;
+};
