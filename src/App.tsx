ed8b6396210373083ba59de5d01019c3f03001b5
Yena-Yun <cheryleduart@gmail.com>
2023-04-05 16:25:27 +0900

    라우터에 로그인/비로그인 페이지 구분하는 Layout 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,30 @@ import { Home } from 'pages/Home';
 import { Login } from 'pages/Login';
 import { Register } from 'pages/Register';
 import { Todo } from 'pages/Todo';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import { HomeLayout } from 'smart-components/HomeLayout';
+import { ProtectedLayout } from 'smart-components/ProtectedLayout';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/signin',
-    element: <Login />,
-  },
-  {
-    path: '/signup',
-    element: <Register />,
-  },
-  {
-    path: '/todo',
-    element: <Todo />,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route element={<HomeLayout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<Login />} />
+        <Route path='/signup' element={<Register />} />
+      </Route>
+
+      <Route element={<ProtectedLayout />}>
+        <Route path='/todo' element={<Todo />} />
+      </Route>
+    </>
+  )
+);
 
 function App() {
   return (
