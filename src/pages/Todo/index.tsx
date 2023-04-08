1d4996b7f703bd824f6f6ae0f012b54a5ea23a69
Yena-Yun <cheryleduart@gmail.com>
2023-04-08 22:21:02 +0900

    Todo 수정 시 기존 내용 그대로 옮겨오도록 처리

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -5,19 +5,14 @@ import React, {
   useRef,
   useState,
 } from 'react';
-import { v4 as uuid4 } from 'uuid';
+import { TodoProps } from 'types/todoType';
+import { todoApi } from 'api/todoApi';
 import { useLocalStorage } from 'hooks/useLocalStorage';
-
-interface TodoProps {
-  id: string;
-  title: string;
-  isCompleted: boolean;
-}
 
 export const Todo = () => {
   const [value, setValue] = useState('');
   const [modifiedValue, setModifiedValue] = useState('');
-  const [isUpdateId, setIsUpdateId] = useState('');
+  const [isUpdateId, setIsUpdateId] = useState<number>(0);
   const createInputRef = useRef<HTMLInputElement>(null);
   const modifyInputRef = useRef<HTMLInputElement>(null);
 
@@ -25,6 +20,7 @@ export const Todo = () => {
 
   useEffect(() => {
     if (storageTodos.length < 1) return;
+
     setStorageTodos(storageTodos);
   }, [storageTodos]);
 
@@ -32,41 +28,49 @@ export const Todo = () => {
     setValue(e.currentTarget.value);
   };
 
-  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (value.length < 1) return;
 
-    const newTodo = {
-      id: uuid4().slice(0, 8),
-      title: value,
-      isCompleted: false,
-    };
+    const { status, data } = await todoApi.createTodo(value);
 
-    setStorageTodos((prev: TodoProps[]) => [...prev, newTodo]);
     setValue('');
+
+    if (status === 201) {
+      setStorageTodos((prev: TodoProps[]) => [...prev, data]);
+    }
   };
 
-  const openUpdate = (id: string) => {
+  const openUpdate = (id: number) => {
     setIsUpdateId(id);
   };
 
   const cancelUpdate = () => {
-    setIsUpdateId('');
+    setIsUpdateId(0);
     setModifiedValue('');
   };
 
-  const updateTodo = (e: React.ChangeEvent<HTMLInputElement>, id: string) => {
+  const updateTodo = (e: React.ChangeEvent<HTMLInputElement>, id: number) => {
     if (isUpdateId === id) {
       setModifiedValue(e.currentTarget.value);
     }
   };
 
-  const deleteTodo = (id: string) => {
+  const deleteTodo = async (id: number) => {
     setStorageTodos(storageTodos.filter((todo: TodoProps) => todo.id !== id));
+    await todoApi.deleteTodo(id);
   };
 
-  const toggleComplete = (id: string) => {
+  const toggleComplete = async (id: number) => {
+    const updateItem =
+      storageTodos.find((prev: TodoProps) => prev.id === id) || null;
+
+    if (!updateItem) {
+      console.log('update할 아이템이 없습니다.');
+      return;
+    }
+
     const updateComplete = storageTodos.map((prev: TodoProps) =>
       prev.id === id
         ? {
@@ -79,7 +83,7 @@ export const Todo = () => {
     setStorageTodos(updateComplete);
   };
 
-  const submitUpdate = (id: string) => {
+  const submitUpdate = async (id: number) => {
     if (modifiedValue.length < 1) {
       cancelUpdate();
       return;
@@ -87,7 +91,7 @@ export const Todo = () => {
 
     const updatedTodo = {
       id,
-      title: modifiedValue,
+      todo: modifiedValue,
       isCompleted: false,
     };
 
@@ -97,8 +101,16 @@ export const Todo = () => {
       )
     );
 
+    const updateItem =
+      storageTodos.find((prev: TodoProps) => prev.id === id) || null;
+
+    if (!updateItem) {
+      console.log('update할 아이템이 없습니다.');
+      return;
+    }
+
     setModifiedValue('');
-    setIsUpdateId('');
+    setIsUpdateId(0);
   };
 
   const inputAutoFocus = useCallback((inputElement: HTMLInputElement) => {
@@ -130,47 +142,45 @@ export const Todo = () => {
 
       {/* Todo 리스트 */}
       <ul>
-        {storageTodos.map((todo: TodoProps) => (
-          <Fragment key={todo.id}>
-            {isUpdateId !== todo.id ? (
-              <li key={todo.id} id={todo.id}>
+        {storageTodos.map(({ id, todo, isCompleted }: TodoProps) => (
+          <Fragment key={id}>
+            {isUpdateId !== id ? (
+              <li key={id}>
                 <label>
                   <input
-                    id={todo.id}
                     type='checkbox'
-                    checked={todo.isCompleted}
-                    onChange={() => toggleComplete(todo.id)}
+                    checked={isCompleted}
+                    onChange={() => toggleComplete(id)}
                   />
-                  <span>{todo.title}</span>
+                  <span>{todo}</span>
                 </label>
                 <button
-                  id={todo.id}
                   data-testid='modify-button'
-                  onClick={() => openUpdate(todo.id)}
+                  onClick={() => openUpdate(id)}
                 >
                   수정
                 </button>
                 <button
                   data-testid='delete-button'
-                  onClick={() => deleteTodo(todo.id)}
+                  onClick={() => deleteTodo(id)}
                 >
                   삭제
                 </button>
               </li>
             ) : (
-              <li key={todo.id} id={todo.id}>
+              <li key={id}>
                 <label>
-                  <input id={todo.id} type='checkbox' disabled />
+                  <input type='checkbox' checked={isCompleted} disabled />
                   <input
                     ref={modifyInputRef}
-                    value={modifiedValue}
+                    value={modifiedValue.length < 1 ? todo : modifiedValue}
                     data-testid='modify-input'
-                    onChange={(e) => updateTodo(e, todo.id)}
+                    onChange={(e) => updateTodo(e, id)}
                   />
                 </label>
                 <button
                   data-testid='submit-button'
-                  onClick={() => submitUpdate(todo.id)}
+                  onClick={() => submitUpdate(id)}
                 >
                   제출
                 </button>
