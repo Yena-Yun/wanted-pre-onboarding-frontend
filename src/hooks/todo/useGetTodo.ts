be959324db76527ecb006631906437b816dcd67d
Yena-Yun <cheryleduart@gmail.com>
2023-04-08 22:20:20 +0900

    useGetTodo와 useTodo 생성 (테스트중)

---
+++ src/hooks/todo/useGetTodo.ts
@@ -0,0 +1,17 @@
+import { todoApi } from 'api/todoApi';
+import { useState } from 'react';
+import { TodoProps } from 'types/todoType';
+
+export const useGetTodo = (): [boolean, TodoProps[], () => Promise<void>] => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [todos, setTodos] = useState<TodoProps[]>([]);
+
+  const getTodos = async () => {
+    const { data } = await todoApi.getTodos();
+
+    setTodos(data);
+    setIsLoading(false);
+  };
+
+  return [isLoading, todos, getTodos];
+};
---
+++ src/hooks/todo/useTodo.tsx
@@ -0,0 +1,46 @@
+import { ReactNode, createContext, useContext, useMemo, useState } from 'react';
+import { todoApi } from 'api/todoApi';
+import { TodoProps } from 'types/todoType';
+
+interface TodoProp {
+  children: ReactNode | ReactNode[];
+}
+
+interface TodoContextProp {
+  isLoading: boolean;
+  todos: TodoProps[];
+  getTodos: () => Promise<void> | void;
+}
+
+const defaultValue = {
+  isLoading: true,
+  todos: [] as TodoProps[],
+  getTodos: () => {},
+};
+
+const TodoContext = createContext<TodoContextProp>(defaultValue);
+
+export const TodoProvider = ({ children }: TodoProp) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [todos, setTodos] = useState<TodoProps[]>([]);
+
+  const getTodos = async (): Promise<void> => {
+    const { data } = await todoApi.getTodos();
+
+    setTodos(data);
+    setIsLoading(false);
+  };
+
+  const value = useMemo(
+    () => ({
+      isLoading,
+      todos,
+      getTodos,
+    }),
+    [isLoading, todos]
+  );
+
+  return <TodoContext.Provider value={value}>{children}</TodoContext.Provider>;
+};
+
+export const useTodo = () => useContext(TodoContext);
