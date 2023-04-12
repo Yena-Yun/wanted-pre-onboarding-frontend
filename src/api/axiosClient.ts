e023d9f9309594fb473c00b10d9b0dbb52829784
Yena-Yun <cheryleduart@gmail.com>
2023-04-12 21:57:21 +0900

    token을 Bearer에 포함한 axiosClient 생성

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,9 +1,19 @@
 import axios from 'axios';
 import { BASE_URL } from './const';
 
+const TOKEN = window.localStorage.getItem('accessToken');
+
 export const axiosClient = axios.create({
   baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
+  },
+});
+
+export const axiosTokenClient = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${TOKEN}`,
   },
 });
